911503169c68207f5677457dbcf370d7fc9926bd
jacobpad <jacobpad@gmail.com>
2019-06-15 22:03:11 -0700

    L08

---
+++ CIT261/assignments/L08/js/week8-team.js
@@ -0,0 +1,88 @@
+function getPeople(url) {
+
+     if (!url) {
+          url = 'https://swapi.co/api/people/';
+     }
+
+     let table = document.querySelector('table');
+
+     fetch(url)
+          .then(
+               (response) => {
+                    response.json()
+                         .then(
+                              (people) => {
+
+                                   //TEMP: delete the console.table
+                                   console.table(people);
+
+                                   //Outputs Names
+                                   people.results.forEach(
+                                        (person) => {
+                                             let row = document.createElement('tr');
+                                             let name = document.createElement('td');
+                                             name.textContent = person.name;
+
+                                             row.appendChild(name);
+                                             table.appendChild(row);
+                                        }
+                                   );
+                                   // Work on Buttons
+                                   if (people.previous === null) {
+                                        document.querySelector('#previous').setAttribute('disabled', 'disabled');
+                                   }
+                                   if (people.next /*=== null*/) {
+                                        document.querySelector('#next').setAttribute('disabled', 'disabled');
+                                   }
+
+                              }
+                         )
+               }
+          )
+          .catch();
+     function prevPg(url = 'https://swapi.co/api/people/') {
+          let p = document.getElementById("previous");
+          p.onclick = () => {
+               prevPg(people.previous);
+          }
+     }
+
+     function nextPg(url = 'https://swapi.co/api/people/') {
+          let n = document.getElementById("next");
+          n.onclick = () => {
+               let nextPg = people.next;
+               nextPg(people.next);
+          }
+     }
+}
+
+
+function showShips(url = 'https://swapi.co/api/people/') {
+     getShips(url).then(function (people) {
+          console.log(people);
+          // const results = data.results;
+
+          //get the list element
+          // const shipListElement = document.getElementById('shiplist');
+          // renderShipList(results, shipListElement);
+
+          // enable the next and prev buttons.
+          if (people.next) {
+               const next = document.getElementById('next');
+               next.ontouchend = () => {
+                    // notice to show the next page we just re-call the showShips function with a new URL
+                    showShips(people.next);
+               };
+          }
+          if (people.previous) {
+               const prev = document.getElementById('prev');
+
+               prev.ontouchend = () => {
+                    showShips(people.previous);
+               };
+          }
+     });
+}
+
+
+// BUTTON TALK STARTS AT 34:30
